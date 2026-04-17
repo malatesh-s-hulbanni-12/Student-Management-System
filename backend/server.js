aed7538c1c39c1e8bb6febd8c85bd549321021b0
malatesh s hulbanni <mshulbanni95@gmail.com>
2026-04-17 10:00:08 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', process.env.FRONTEND_URL],
+  origin: ['http://localhost:5173', 'http://localhost:3000', process.env.FRONTEND_URL,'https://student-management-system-duan.vercel.app'],
   credentials: true
 }))
 app.use(express.json())
